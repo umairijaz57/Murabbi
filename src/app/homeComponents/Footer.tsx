7b47b8dd7ce2a4170668aebb500c247f3068bf61
umairijaz62 <umairijaz2772@gmail.com>
2025-07-31 16:48:49 +0500

    check 2

--- src/app/homeComponents/Footer.tsx
+++ src/app/homeComponents/Footer.tsx
@@ -8,33 +8,39 @@ interface Props {
 
 const Footer = (props: Props) => {
   return (
-    <footer className={` bg-blue-600 text-white `}>
+    <footer className="bg-blue-600 text-white">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-        <div className="md:flex md:justify-between ">
-          <div className="mb-6 md:mb-0 flex flex-col place-items-center justify-center gap-4 ">
-            <a href="/" className="flex items-center  ">
-              <Image src="/logo-footer.png" alt="" width={120} height={120} />
-            </a>
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0 flex flex-col place-items-center justify-center gap-4">
+            <Link href="/" className="flex items-center">
+              <Image src="/logo-footer.png" alt="Murabbi Logo" width={120} height={120} />
+            </Link>
             <div>
-              <div className="flex space-x-6 sm:justify-center ">
+              <div className="flex space-x-6 sm:justify-center">
                 <a
+                suppressHydrationWarning
                   href="https://www.facebook.com/profile.php?id=61558689572370"
                   target="_blank"
-                  className=" flex justify-center place-items-center h-6 w-6 rounded-full bg-white "
+                  rel="noopener noreferrer"
+                  className="flex justify-center place-items-center h-6 w-6 rounded-full bg-white"
                 >
                   <FaFacebookF className="text-[1rem] text-blue-600" />
                 </a>
                 <a
+                suppressHydrationWarning
                   href="https://www.instagram.com/murabbi.io/"
                   target="_blank"
-                  className=" flex justify-center place-items-center h-6 w-6 rounded-full bg-white "
+                  rel="noopener noreferrer"
+                  className="flex justify-center place-items-center h-6 w-6 rounded-full bg-white"
                 >
                   <FaInstagram className="text-[1.1rem] text-blue-600" />
                 </a>
                 <a
+                suppressHydrationWarning
                   href="https://www.linkedin.com/company/murabbi/"
                   target="_blank"
-                  className=" flex justify-center place-items-center h-6 w-6 rounded-full bg-white "
+                  rel="noopener noreferrer"
+                  className="flex justify-center place-items-center h-6 w-6 rounded-full bg-white"
                 >
                   <FaLinkedinIn className="text-[1.1rem] text-blue-600" />
                 </a>
@@ -43,31 +49,29 @@ const Footer = (props: Props) => {
           </div>
           <div className="flex flex-col md:gap-20 md:flex-row md:mt-4">
             <div>
-              <ul className=" font-medium text-center md:text-left">
-                <li className="mb-2 mt-6 md:mt-0 md:mb-4 text-xl font-bold ">
-                  <a href="/aboutus" className="hover:underline">
+              <ul className="font-medium text-center md:text-left">
+                <li className="mb-2 mt-6 md:mt-0 md:mb-4 text-xl font-bold">
+                  <Link href="/aboutus" className="hover:underline">
                     About Us
-                  </a>
+                  </Link>
                 </li>
                 <Link href="/aboutus#teams-section">
                   <li className="mb-4 text-sm">Our Team</li>
                 </Link>
-
                 <Link href="/aboutus#history-section">
                   <li className="mb-4 text-sm">Our History</li>
                 </Link>
-
                 <Link href="/aboutus#partners-section">
                   <li className="text-sm">Partners</li>
                 </Link>
               </ul>
             </div>
             <div>
-              <ul className=" font-medium text-center md:text-left">
-                <li className="mb-2 mt-8 md:mt-0 md:mb-4 text-xl font-bold ">
-                  <a href="/resources" className="hover:underline">
+              <ul className="font-medium text-center md:text-left">
+                <li className="mb-2 mt-8 md:mt-0 md:mb-4 text-xl font-bold">
+                  <Link href="/resources" className="hover:underline">
                     Resources
-                  </a>
+                  </Link>
                 </li>
                 <Link href="/news">
                   <li className="mb-4 text-sm">News</li>
@@ -80,19 +84,18 @@ const Footer = (props: Props) => {
                 </Link>
               </ul>
             </div>
-
             <div>
-              <ul className=" font-medium text-center md:text-left">
-                <li className="mb-2 mt-8 md:mt-0 md:mb-4 text-xl font-bold ">
-                  <a href="/offer" className="hover:underline">
+              <ul className="font-medium text-center md:text-left">
+                <li className="mb-2 mt-8 md:mt-0 md:mb-4 text-xl font-bold">
+                  <Link href="/offer" className="hover:underline">
                     What We Offer
-                  </a>
+                  </Link>
                 </li>
                 <Link href="/whatweoffer/robotics">
                   <li className="mb-4 text-sm">Robotics</li>
                 </Link>
                 <Link href="/whatweoffer/artificialintelligence">
-                  <li className="mb-4 text-sm">Artificail Intelligence</li>
+                  <li className="mb-4 text-sm">Artificial Intelligence</li>
                 </Link>
                 <Link href="/whatweoffer/cybersecurity">
                   <li className="text-sm mb-4">Cyber Security</li>
@@ -103,17 +106,16 @@ const Footer = (props: Props) => {
               </ul>
             </div>
             <div>
-              <ul className=" font-medium text-center md:text-left">
-                <li className="mb-2 mt-8 md:mt-0 md:mb-4 text-xl font-bold ">
-                  <a href="/contactus" className="hover:underline">
+              <ul className="font-medium text-center md:text-left">
+                <li className="mb-2 mt-8 md:mt-0 md:mb-4 text-xl font-bold">
+                  <Link href="/contactus" className="hover:underline">
                     Contact Us
-                  </a>
+                  </Link>
                 </li>
                 <li className="mb-4 text-sm">+92 333 1555665</li>
                 <li className="mb-4 text-sm">
-                  {" "}
                   <Link
-                    href="mailto:murabbi.socials@gmail.coms"
+                    href="mailto:murabbi.socials@gmail.com"
                     className="underline"
                   >
                     <span>murabbi.socials@gmail.com</span>
@@ -128,13 +130,13 @@ const Footer = (props: Props) => {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
-          {/* <span className="text-sm t sm:text-center ">
-            © 2023{" "}
-            <a href="#" className="hover:underline">
-              Flowbite™
-            </a>
+          <span className="text-sm sm:text-center">
+            © 2025{" "}
+            <Link href="/" className="hover:underline">
+              Murabbi
+            </Link>
             . All Rights Reserved.
-          </span> */}
+          </span>
         </div>
       </div>
     </footer>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
-      <body>{children}</body>
+      <body suppressHydrationWarning={true}>{children}</body>
     </html>
   );
 }
